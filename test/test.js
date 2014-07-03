d4c4d0e91ca7ffae344f1c9c7a11b95deee7ff66
Charles Pick <charles.pick@gmail.com>
2014-07-03 13:15:41 +0100

    refactor tests to use collections

--- test/test.js
+++ test/test.js
@@ -9,6 +9,10 @@ var Promise = require('bluebird'),
 
 
 describe("Integration Tests", function () {
+  var collection;
+  before(function () {
+    collection = LIB.dir(__dirname + '/input');
+  });
   run("nothing");
   run("simple");
   run("page");
@@ -23,34 +27,33 @@ describe("Integration Tests", function () {
   run("layout");
   run("optional");
   run("filter");
-});
 
 
-function run (name) {
-  it("should process " + name + ".html", function () {
-    return Promise.all([
-      fs.readFileAsync(__dirname + '/input/page.html', 'utf8'),
-      fs.readFileAsync(__dirname + '/input/' + name + '.html', 'utf8'),
-      fs.readFileAsync(__dirname + '/expected/' + name + '.html', 'utf8')
-    ])
-    .spread(function (layout, input, expected) {
-      var template = LIB.string(input);
-      template.layouts.set('page.html', LIB.string(layout));
-      //console.log(template.render+'')
-      // console.log(template.render(fixtures));
-      var content = '';
-      if (/-with-content$/.test(name)) {
-        content = '<p>Content</p>';
-      }
 
-      template.toCase = function (direction, input) {
-        return input['to' + direction.charAt(0).toUpperCase() + direction.slice(1) + 'Case']();
-      };
+  function run (name) {
+    it("should process " + name + ".html", function () {
+      return Promise.all([
+        fs.readFileAsync(__dirname + '/expected/' + name + '.html', 'utf8')
+      ])
+      .spread(function (expected) {
+        var template = collection.get(name + '.html');
 
-      template.reverse = function (input) {
-        return input.split('').reverse().join('');
-      };
-      template.render(fixtures, content).should.equal(expected);
+        //console.log(template.render+'')
+        // console.log(template.render(fixtures));
+        var content = '';
+        if (/-with-content$/.test(name)) {
+          content = '<p>Content</p>';
+        }
+
+        template.toCase = function (direction, input) {
+          return input['to' + direction.charAt(0).toUpperCase() + direction.slice(1) + 'Case']();
+        };
+
+        template.reverse = function (input) {
+          return input.split('').reverse().join('');
+        };
+        template.render(fixtures, content).should.equal(expected);
+      });
     });
-  });
-}
+  }
+});
