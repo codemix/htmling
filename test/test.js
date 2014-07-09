5576ce8af184b5a27873db8bc14f8d978db08df0
Charles Pick <charles.pick@gmail.com>
2014-07-09 22:41:29 +0100

    fix bug with repeat include object

--- test/test.js
+++ test/test.js
@@ -29,6 +29,7 @@ describe("Integration Tests", function () {
   run("include-with-bind");
   run("include-with-bind-alias");
   run("include-repeat");
+  run("include-repeat-object");
   run("include-repeat-no-content");
   run("include-repeat-alias");
   run("include-repeat-alias-index");
